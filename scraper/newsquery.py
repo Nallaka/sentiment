79635b1da6cb0e65777bb6ddabb555184b98fe1e
Nallaka <mithul0700@gmail.com>
2024-04-28 16:54:42 -0500

    final

--- scraper/newsquery.py
+++ scraper/newsquery.py
@@ -5,23 +5,26 @@ import json
 
 nashvile_queries = ["nashville opioid abuse", "nashville opioid overdose", "nashville opiod deaths"]
 
-for query in nashvile_queries:
-    params = {
-        "engine": "google_news",
-        "q": query,
-        "api_key": "42b0d9e716ea8d286b73e1a0628e3bde3ef019f62296a5500da8544edf964c55"
-    }
+places = [nashvile_queries]
 
-    searchresults = search(params)
-    nashville_results = searchresults.as_dict()
-    nashville_news_results = nashville_results["news_results"]
+for place in places:
+    for query in place:
+        params = {
+            "engine": "google_news",
+            "q": query,
+            "api_key": "42b0d9e716ea8d286b73e1a0628e3bde3ef019f62296a5500da8544edf964c55"
+        }
 
-    print(nashville_news_results)
-    print(len(nashville_news_results))
+        searchresults = search(params)
+        results_dict = searchresults.as_dict()
+        news_results = results_dict["news_results"]
 
-    with open('news_data/nashville_news_links.json', 'a') as file:
-        json_data = json.dumps(nashville_news_results)
+        print(news_results)
+        print(len(news_results))
 
-        file.write(json_data + '\n')
+        with open('/content/news_data/nashville_news_links.json', 'a') as file:
+            json_data = json.dumps(news_results)
 
-    time.sleep(5)
+            file.write(json_data + '\n')
+
+        time.sleep(2)
